3f2093ef8f1ec4cc4d6469c3c143944f686880c4
c <c@c.Home>
2018-03-01 02:53:26 -0600

    updates

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,71 @@
+var store = [{
+        "title": "1",
+        "excerpt":"1","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/1/",
+        "teaser":null},{
+        "title": "2",
+        "excerpt":"2","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/2/",
+        "teaser":null},{
+        "title": "3",
+        "excerpt":"3","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/3/",
+        "teaser":null},{
+        "title": "eight",
+        "excerpt":"eight","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/eight/",
+        "teaser":null},{
+        "title": "eleven",
+        "excerpt":"eleven","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/eleven/",
+        "teaser":null},{
+        "title": "five",
+        "excerpt":"five","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/five/",
+        "teaser":null},{
+        "title": "four",
+        "excerpt":"four","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/four/",
+        "teaser":null},{
+        "title": "nine",
+        "excerpt":"nine","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/nine/",
+        "teaser":null},{
+        "title": "one",
+        "excerpt":"one","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/one/",
+        "teaser":null},{
+        "title": "seven",
+        "excerpt":"seven","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/seven/",
+        "teaser":null},{
+        "title": "sixth",
+        "excerpt":"sixth","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/six/",
+        "teaser":null},{
+        "title": "ten",
+        "excerpt":"ten","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/ten/",
+        "teaser":null},{
+        "title": "three",
+        "excerpt":"three","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/three/",
+        "teaser":null},{
+        "title": "two",
+        "excerpt":"two","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/two/",
+        "teaser":null}]
